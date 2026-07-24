6219fd3e2f5b27767ad31bbe3851bacd70f88521
xaltsc <hey+dev@xaltsc.dev>
2026-07-24 19:25:46 +0200

    flake: just reexpose c10y directly without doing anything

--- flake.nix
+++ flake.nix
@@ -5,19 +5,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-
-  outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      perSystem = { inputs', ... }: {
-        inherit (inputs'.c10y) packages;
-      };
-
-    };
+  outputs = { c10y, ... }: c10y.outputs;
 }
